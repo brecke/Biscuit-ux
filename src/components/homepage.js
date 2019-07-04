7b436b7fb7f24bf9bda729b152a0061a9e5ac555
Miguel Laginha <me@miguellaginha.com>
2019-07-04 10:23:39 +0100

    chore: experimental support for material components web

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,10 +1,12 @@
-import {html, css} from 'lit-element';
-import {PageViewElement} from './page-view-element.js';
-// import {SharedStyles} from './shared-styles.js';
-
-import homepageStyles from './../../style/homepage.scss';
-import sharedStyles from './../../style/app.scss';
+import { html, css } from 'lit-element';
+// Import {SharedStyles} from './shared-styles.js';
+import { MDCTextField } from '@material/textfield';
+import { MDCRipple } from '@material/ripple';
 import legacyStyles from '../../style/legacy.css';
+
+import homepageStyles from '../../style/homepage.scss';
+import sharedStyles from '../../style/app.scss';
+import { PageViewElement } from './page-view-element.js';
 
 class OAEHomepage extends PageViewElement {
   static get styles() {
@@ -19,9 +21,34 @@ class OAEHomepage extends PageViewElement {
       <style include="app"></style>
       <section>
         <h2>This is the homepage</h2>
-        <button class="foo-button mdc-button">Button</button>
+        <button class="foo-button mdc-button">Submit</button>
+
+        <div class="mdc-text-field">
+          <input type="text" id="my-text-field" class="mdc-text-field__input" />
+          <label class="mdc-floating-label" for="my-text-field">Label</label>
+          <div class="mdc-line-ripple"></div>
+        </div>
       </section>
     `;
   }
+
+  connectedCallback() {
+    super.connectedCallback();
+    console.log('Just connected to the DOM!');
+  }
+
+  firstUpdated() {
+    console.log('First updated!');
+
+    // Enable ripple effect on button
+    console.dir(myButton);
+    const myButton = this.shadowRoot.querySelector('.foo-button');
+    const ripple = new MDCRipple(myButton);
+
+    // enable effects on textfield
+    const myTextField = this.shadowRoot.querySelector('.mdc-text-field');
+    const textField = new MDCTextField(myTextField);
+  }
 }
+
 window.customElements.define('oae-landing-page', OAEHomepage);
